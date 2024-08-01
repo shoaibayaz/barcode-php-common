d32a54dc6f749a64b9b83cbb76e64a5806e2c143
Muhammad Shoaib <shoaib.ayaz@yahoo.com>
2024-08-01 21:25:52 +0500

    wip

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'barcode-bakery/barcode-common',
+        'pretty_version' => '7.0.4',
+        'version' => '7.0.4.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'barcode-bakery/barcode-common' => array(
+            'pretty_version' => '7.0.4',
+            'version' => '7.0.4.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
